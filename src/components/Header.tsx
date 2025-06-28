8189605d1fbe05e3cac5373abd6613ae993facd6
agathe <agathe@agat.dev>
2025-06-28 06:11:54 +0200

    logo contain

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ const Header = () => {
                 <img 
                   src="/logo.png"
                   alt="Logo"
-                  className="w-20 h-20"
+                  className="w-20 h-20 object-contain"
                 />
               </span>
             </div>
